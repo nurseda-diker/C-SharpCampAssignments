0e86a7203ccf13bba9e7212ac625e22cc70514da
Nurseda Diker <54947744+nurseda-diker@users.noreply.github.com>
2023-03-15 22:28:32 +0300

    ödev 1

---
+++ ClassMetotDemo/ClassMetotDemo/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public double  TcNo { get; set; }
+
+    }
+}
---
+++ ClassMetotDemo/ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+        public void AddCustomer(Customer customer)
+        {
+            Console.WriteLine("Müşteri eklendi"+customer.Name); 
+        }
+        public void CustomerList(Customer[] customers)
+        {
+            foreach(var customer in customers)
+            {
+                Console.WriteLine("Müşteriler listelendi" + customer.Name);
+            }
+            
+            
+        }
+
+        public void DeleteCustomer(Customer customer)
+        {
+            Console.WriteLine("Müşteri silindi" + customer.Name);
+        }
+    }
+}
